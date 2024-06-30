351e1ce99580d93082bce97bddf7dc0cb7020746
Hiromasa Ihara <iharahiromasa@gmail.com>
2024-06-30 21:03:28 +0900

    fix: update example

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,3 +1,5 @@
+this is example.
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from 'src/environments';
